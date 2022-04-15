5b35cd3522adf58c9928f2592d7095455bac349e
Tushar Muralidharan <u7253634@anu.edu.au>
2022-04-16 09:10:15 +1000

    Enable dark mode by default

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
-chrome.action.onClicked.addListener(tab => {
+chrome.storage.sync.get(['darkMode'], function(result) {
+  if (!result.darkMode) chrome.storage.sync.set({ darkMode: true })
+})
+
+chrome.action.onClicked.addListener(() => {
   chrome.storage.sync.get(['darkMode'], function(result) {
-    if (result.darkMode) chrome.action.setIcon({ path: 'img/icon32.png' })
+    if (result.darkMode) chrome.action.setIcon({ path: 'img/icon32-disabled.png' })
     else chrome.action.setIcon({ path: 'img/icon128.png' })
     chrome.storage.sync.set({ darkMode: !result.darkMode })
   })
