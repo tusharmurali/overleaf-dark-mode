0d5ba9c9abfa9db912f865809b04ffe8b3131d21
Tushar Muralidharan <u7253634@anu.edu.au>
2024-03-19 16:59:33 +1100

    Update to Overleaf redesign

--- toggle.js
+++ toggle.js
@@ -7,18 +7,42 @@ style.innerText = `html, .loading-screen.ng-scope, .history-entries, .history-la
     background-color: #e4e8ed;
 }
 
+/* 
 .cm-editor,
 .cm-panels-top,
-.popover,
-.pdf-viewer,   
-.site-footer,   
-.history-react,
-.modal, 
-.dropdown-menu,  
+.pdf-viewer,
 .project-list-main-react,
+.material-symbols,
+.toolbar,
+.toolbar-pdf .dropdown-menu,
+div[role="separator"],
+.ide-react-editor-sidebar,
+.site-footer,
+*/
+
+body,
+${location.href.endsWith("overleaf.com/") || location.href.endsWith("overleaf.com") ? "" : ".navbar, .navbar .dropdown-menu,"}
+footer,
+.popover,
+.tooltip,
+.pdf,
+.pdf-viewer,
+.cm-panels-top,
+.toolbar-header,
+.ide-react-editor-sidebar,
+.history-react,
+.toolbar .dropdown-menu,
+.modal-backdrop,
+.project-list-sidebar-wrapper-react,
 .project-list-main-react .fa-circle,
-.loading-panel,
-.loading-screen {
+.synctex-controls,
+.custom-toggler,
+.doc-container,
+.tag-dot,
+.color-picker-item,
+.loading,
+.history-version-list-container,
+.chat {
     filter: url('data:image/svg+xml, <svg xmlns="http://www.w3.org/2000/svg">\
       <filter id="invert" color-interpolation-filters="linearRGB">\
         <feColorMatrix in="SourceGraphic" type="matrix"\
@@ -30,11 +54,38 @@ style.innerText = `html, .loading-screen.ng-scope, .history-entries, .history-la
     </svg>#invert');
 }
 
-.project-list-main-react {
-    background-color: #f4f5f6;
-}`
+.cm-gutters {
+    background-color: transparent !important;
+}
 
-// TODO: fix cm-gutters (line numbers), history panel, and ide-react-panel (backgrounds when resizing)
+.cm-panels-top {
+    border-bottom: 1px solid #222 !important;
+}
+
+.project-list-main-react, .cm-activeLineGutter {
+    background-color: #f4f5f6 !important;
+}
+
+.horizontal-resize-handle {
+    background-color: #fcfaf7;
+}
+
+.doc-container .loading {
+    background-color: #000000;
+    color: #ffffff;
+}
+
+.doc-container, .history-all-versions-container, .loading, #left-menu {
+    background-color: #ffffff;
+}
+
+.cm-activeLine {
+    background: rgba(0, 0, 0, 0.02) !important;
+}
+
+::selection, .ͼe.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground, .ͼe .cm-selectionBackground, .ͼe .cm-content ::selection, .ͼe .cm-searchMatch.cm-searchMatch.cm-searchMatch-selected {
+    background-color: rgba(181, 213, 255, 0.3);
+}`
 
 chrome.storage.sync.get(['darkMode'], function(result) {
     if (result.darkMode) document.body.appendChild(style)
